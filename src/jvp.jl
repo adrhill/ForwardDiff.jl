89b3c049378d83c95106e0182f1c2273da7a8ddb
Adrian Hill <adrian.hill@mailbox.org>
2024-01-22 17:16:28 +0100

    Define API
    modify `jacobian` template for `jvp`.
    
    Rename errors

--- src/jvp.jl
+++ src/jvp.jl
@@ -2,97 +2,100 @@
 # API methods #
 ###############
 
-"""
-    ForwardDiff.jacobian(f, x::AbstractArray, cfg::JacobianConfig = JacobianConfig(f, x), check=Val{true}())
+# Related issues: #319, #428, #487
 
-Return `J(f)` evaluated at `x`, assuming `f` is called as `f(x)`.
+"""
+    ForwardDiff.jvp(f, x::AbstractArray, dx::AbstractArray, cfg::JVPConfig = JVPConfig(f, x, dx), check=Val{true}())
+
+Return `J(f) ⋅ dx` (where `J(f)` is evaluated at `x`), assuming `f` is called as `f(x)`.
 Multidimensional arrays are flattened in iteration order: the array
 `J(f)` has shape `length(f(x)) × length(x)`, and its elements are
-`J(f)[j,k] = ∂f(x)[j]/∂x[k]`.  When `x` is a vector, this means
-that `jacobian(x->[f(x)], x)` is the transpose of `gradient(f, x)`.
+`J(f)[j,k] = ∂f(x)[j]/∂x[k]`.
+`J(f) ⋅ dx` therefore has shape `length(f(x))`.
 
 This method assumes that `isa(f(x), AbstractArray)`.
 
 Set `check` to `Val{false}()` to disable tag checking. This can lead to perturbation confusion, so should be used with care.
 """
-function jacobian(f::F, x::AbstractArray, cfg::JacobianConfig{T} = JacobianConfig(f, x), ::Val{CHK}=Val{true}()) where {F,T,CHK}
+function jvp(f::F, x::AbstractArray, dx::AbstractArray, cfg::JVPConfig{T} = JVPConfig(f, x, dx), ::Val{CHK}=Val{true}()) where {F,T,CHK}
     require_one_based_indexing(x)
     CHK && checktag(T, f, x)
     if chunksize(cfg) == length(x)
-        return vector_mode_jacobian(f, x, cfg)
+        return vector_mode_jvp(f, x, cfg)
     else
-        return chunk_mode_jacobian(f, x, cfg)
+        return chunk_mode_jvp(f, x, cfg)
     end
 end
 
 """
-    ForwardDiff.jacobian(f!, y::AbstractArray, x::AbstractArray, cfg::JacobianConfig = JacobianConfig(f!, y, x), check=Val{true}())
+    ForwardDiff.jvp(f!, y::AbstractArray, x::AbstractArray, dx::AbstractArray, cfg::JVPConfig = JVPConfig(f!, y, x, dx), check=Val{true}())
 
-Return `J(f!)` evaluated at `x`,  assuming `f!` is called as `f!(y, x)` where the result is
+Return `J(f!) ⋅ dx` (where `J(f!)` is evaluated at `x`),  assuming `f!` is called as `f!(y, x)` where the result is
 stored in `y`.
 
 Set `check` to `Val{false}()` to disable tag checking. This can lead to perturbation confusion, so should be used with care.
 """
-function jacobian(f!::F, y::AbstractArray, x::AbstractArray, cfg::JacobianConfig{T} = JacobianConfig(f!, y, x), ::Val{CHK}=Val{true}()) where {F,T, CHK}
+function jvp(f!::F, y::AbstractArray, x::AbstractArray, dx::AbstractArray, cfg::JVPConfig{T} = JVPConfig(f!, y, x, dx), ::Val{CHK}=Val{true}()) where {F,T, CHK}
     require_one_based_indexing(y, x)
     CHK && checktag(T, f!, x)
     if chunksize(cfg) == length(x)
-        return vector_mode_jacobian(f!, y, x, cfg)
+        return vector_mode_jvp(f!, y, x, cfg)
     else
-        return chunk_mode_jacobian(f!, y, x, cfg)
+        return chunk_mode_jvp(f!, y, x, cfg)
     end
 end
 
 
 """
-    ForwardDiff.jacobian!(result::Union{AbstractArray,DiffResult}, f, x::AbstractArray, cfg::JacobianConfig = JacobianConfig(f, x), check=Val{true}())
+    ForwardDiff.jvp!(result::Union{AbstractArray,DiffResult}, f, x::AbstractArray, dx::AbstractArray, cfg::JVPConfig = JVPConfig(f, x, dx), check=Val{true}())
 
-Compute `J(f)` evaluated at `x` and store the result(s) in `result`, assuming `f` is called
+Compute `J(f) ⋅ dx` (where `J(f)` is evaluated at `x`) and store the result(s) in `result`, assuming `f` is called
 as `f(x)`.
 
 This method assumes that `isa(f(x), AbstractArray)`.
 
 Set `check` to `Val{false}()` to disable tag checking. This can lead to perturbation confusion, so should be used with care.
 """
-function jacobian!(result::Union{AbstractArray,DiffResult}, f::F, x::AbstractArray, cfg::JacobianConfig{T} = JacobianConfig(f, x), ::Val{CHK}=Val{true}()) where {F,T, CHK}
+function jvp!(result::Union{AbstractArray,DiffResult}, f::F, x::AbstractArray, dx::AbstractArray, cfg::JVPConfig{T} = JVPConfig(f, x, dx), ::Val{CHK}=Val{true}()) where {F,T, CHK}
     result isa DiffResult ? require_one_based_indexing(x) : require_one_based_indexing(result, x)
     CHK && checktag(T, f, x)
     if chunksize(cfg) == length(x)
-        vector_mode_jacobian!(result, f, x, cfg)
+        vector_mode_jvp!(result, f, x, cfg)
     else
-        chunk_mode_jacobian!(result, f, x, cfg)
+        chunk_mode_jvp!(result, f, x, cfg)
     end
     return result
 end
 
 """
-    ForwardDiff.jacobian!(result::Union{AbstractArray,DiffResult}, f!, y::AbstractArray, x::AbstractArray, cfg::JacobianConfig = JacobianConfig(f!, y, x), check=Val{true}())
+    ForwardDiff.jvp!(result::Union{AbstractArray,DiffResult}, f!, y::AbstractArray, x::AbstractArray, dx::AbstractArray, cfg::JVPConfig = JVPConfig(f!, y, x, dx), check=Val{true}())
 
-Compute `J(f!)` evaluated at `x` and store the result(s) in `result`, assuming `f!` is
+Compute `J(f) ⋅ dx` (where `J(f)` is evaluated at `x`) and store the result(s) in `result`, assuming `f!` is
 called as `f!(y, x)` where the result is stored in `y`.
 
 This method assumes that `isa(f(x), AbstractArray)`.
 
 Set `check` to `Val{false}()` to disable tag checking. This can lead to perturbation confusion, so should be used with care.
 """
-function jacobian!(result::Union{AbstractArray,DiffResult}, f!::F, y::AbstractArray, x::AbstractArray, cfg::JacobianConfig{T} = JacobianConfig(f!, y, x), ::Val{CHK}=Val{true}()) where {F,T,CHK}
+function jvp!(result::Union{AbstractArray,DiffResult}, f!::F, y::AbstractArray, x::AbstractArray, dx::AbstractArray, cfg::JVPConfig{T} = JVPConfig(f!, y, x, dx), ::Val{CHK}=Val{true}()) where {F,T,CHK}
     result isa DiffResult ? require_one_based_indexing(y, x) : require_one_based_indexing(result, y, x)
     CHK && checktag(T, f!, x)
     if chunksize(cfg) == length(x)
-        vector_mode_jacobian!(result, f!, y, x, cfg)
+        vector_mode_jvp!(result, f!, y, x, cfg)
     else
-        chunk_mode_jacobian!(result, f!, y, x, cfg)
+        chunk_mode_jvp!(result, f!, y, x, cfg)
     end
     return result
 end
 
-jacobian(f, x::Real) = throw(DimensionMismatch("jacobian(f, x) expects that x is an array. Perhaps you meant derivative(f, x)?"))
+# TODO: scalar inputs
+jvp(f, x::Real, dx::Real) = throw(DimensionMismatch("jvp(f, x, dx) expects that x is an array. Perhaps you meant `derivative(f, x) * dx`?"))
 
 #####################
 # result extraction #
 #####################
 
-function extract_jacobian!(::Type{T}, result::AbstractArray, ydual::AbstractArray, n) where {T}
+function extract_jvp!(::Type{T}, result::AbstractArray, ydual::AbstractArray, n) where {T}
     out_reshaped = reshape(result, length(ydual), n)
     ydual_reshaped = vec(ydual)
     # Use closure to avoid GPU broadcasting with Type
@@ -101,12 +104,12 @@ function extract_jacobian!(::Type{T}, result::AbstractArray, ydual::AbstractArra
     return result
 end
 
-function extract_jacobian!(::Type{T}, result::MutableDiffResult, ydual::AbstractArray, n) where {T}
-    extract_jacobian!(T, DiffResults.jacobian(result), ydual, n)
+function extract_jvp!(::Type{T}, result::MutableDiffResult, ydual::AbstractArray, n) where {T}
+    extract_jvp!(T, DiffResults.jvp(result), ydual, n)
     return result
 end
 
-function extract_jacobian_chunk!(::Type{T}, result, ydual, index, chunksize) where {T}
+function extract_jvp_chunk!(::Type{T}, result, ydual, index, chunksize) where {T}
     ydual_reshaped = vec(ydual)
     offset = index - 1
     irange = 1:chunksize
@@ -117,56 +120,57 @@ function extract_jacobian_chunk!(::Type{T}, result, ydual, index, chunksize) whe
     return result
 end
 
-reshape_jacobian(result, ydual, xdual) = reshape(result, length(ydual), length(xdual))
-reshape_jacobian(result::DiffResult, ydual, xdual) = reshape_jacobian(DiffResults.jacobian(result), ydual, xdual)
+reshape_jvp(result, ydual, xdual) = reshape(result, length(ydual), length(xdual))
+reshape_jvp(result::DiffResult, ydual, xdual) = reshape_jvp(DiffResults.jvp(result), ydual, xdual)
 
 ###############
 # vector mode #
 ###############
 
-function vector_mode_jacobian(f::F, x, cfg::JacobianConfig{T}) where {F,T}
+function vector_mode_jvp(f::F, x, cfg::JVPConfig{T}) where {F,T}
     N = chunksize(cfg)
     ydual = vector_mode_dual_eval!(f, cfg, x)
-    ydual isa AbstractArray || throw(JACOBIAN_ERROR)
+    ydual isa AbstractArray || throw(JVP_ERROR)
     result = similar(ydual, valtype(eltype(ydual)), length(ydual), N)
-    extract_jacobian!(T, result, ydual, N)
+    extract_jvp!(T, result, ydual, N)
     extract_value!(T, result, ydual)
     return result
 end
 
-function vector_mode_jacobian(f!::F, y, x, cfg::JacobianConfig{T}) where {F,T}
+function vector_mode_jvp(f!::F, y, x, cfg::JVPConfig{T}) where {F,T}
     N = chunksize(cfg)
     ydual = vector_mode_dual_eval!(f!, cfg, y, x)
     map!(d -> value(T,d), y, ydual)
     result = similar(y, length(y), N)
-    extract_jacobian!(T, result, ydual, N)
+    extract_jvp!(T, result, ydual, N)
     map!(d -> value(T,d), y, ydual)
     return result
 end
 
-function vector_mode_jacobian!(result, f::F, x, cfg::JacobianConfig{T}) where {F,T}
+function vector_mode_jvp!(result, f::F, x, cfg::JVPConfig{T}) where {F,T}
     N = chunksize(cfg)
     ydual = vector_mode_dual_eval!(f, cfg, x)
-    extract_jacobian!(T, result, ydual, N)
+    extract_jvp!(T, result, ydual, N)
     extract_value!(T, result, ydual)
     return result
 end
 
-function vector_mode_jacobian!(result, f!::F, y, x, cfg::JacobianConfig{T}) where {F,T}
+function vector_mode_jvp!(result, f!::F, y, x, cfg::JVPConfig{T}) where {F,T}
     N = chunksize(cfg)
     ydual = vector_mode_dual_eval!(f!, cfg, y, x)
     map!(d -> value(T,d), y, ydual)
-    extract_jacobian!(T, result, ydual, N)
+    extract_jvp!(T, result, ydual, N)
     extract_value!(T, result, y, ydual)
     return result
 end
 
-const JACOBIAN_ERROR = DimensionMismatch("jacobian(f, x) expects that f(x) is an array. Perhaps you meant gradient(f, x)?")
+# TODO: scalar outputs
+const JVP_ERROR = DimensionMismatch("jvp(f, x, dx) expects that f(x) is an array. Perhaps you meant gradient(f, x)?")
 
 # chunk mode #
 #------------#
 
-function jacobian_chunk_mode_expr(work_array_definition::Expr, compute_ydual::Expr,
+function jvp_chunk_mode_expr(work_array_definition::Expr, compute_ydual::Expr,
                                   result_definition::Expr, y_definition::Expr)
     return quote
         @assert length(x) >= N "chunk size cannot be greater than length(x) ($(N) > $(length(x)))"
@@ -185,10 +189,10 @@ function jacobian_chunk_mode_expr(work_array_definition::Expr, compute_ydual::Ex
         # do first chunk manually to calculate output type
         seed!(xdual, x, 1, seeds)
         $(compute_ydual)
-        ydual isa AbstractArray || throw(JACOBIAN_ERROR)
+        ydual isa AbstractArray || throw(JVP_ERROR)
         $(result_definition)
-        out_reshaped = reshape_jacobian(result, ydual, xdual)
-        extract_jacobian_chunk!(T, out_reshaped, ydual, 1, N)
+        out_reshaped = reshape_jvp(result, ydual, xdual)
+        extract_jvp_chunk!(T, out_reshaped, ydual, 1, N)
         seed!(xdual, x, 1)
 
         # do middle chunks
@@ -196,14 +200,14 @@ function jacobian_chunk_mode_expr(work_array_definition::Expr, compute_ydual::Ex
             i = ((c - 1) * N + 1)
             seed!(xdual, x, i, seeds)
             $(compute_ydual)
-            extract_jacobian_chunk!(T, out_reshaped, ydual, i, N)
+            extract_jvp_chunk!(T, out_reshaped, ydual, i, N)
             seed!(xdual, x, i)
         end
 
         # do final chunk
         seed!(xdual, x, lastchunkindex, seeds, lastchunksize)
         $(compute_ydual)
-        extract_jacobian_chunk!(T, out_reshaped, ydual, lastchunkindex, lastchunksize)
+        extract_jvp_chunk!(T, out_reshaped, ydual, lastchunkindex, lastchunksize)
 
         $(y_definition)
 
@@ -211,8 +215,8 @@ function jacobian_chunk_mode_expr(work_array_definition::Expr, compute_ydual::Ex
     end
 end
 
-@eval function chunk_mode_jacobian(f::F, x, cfg::JacobianConfig{T,V,N}) where {F,T,V,N}
-    $(jacobian_chunk_mode_expr(quote
+@eval function chunk_mode_jvp(f::F, x, cfg::JVPConfig{T,V,N}) where {F,T,V,N}
+    $(jvp_chunk_mode_expr(quote
                                    xdual = cfg.duals
                                    seed!(xdual, x)
                                end,
@@ -221,8 +225,8 @@ end
                                :()))
 end
 
-@eval function chunk_mode_jacobian(f!::F, y, x, cfg::JacobianConfig{T,V,N}) where {F,T,V,N}
-    $(jacobian_chunk_mode_expr(quote
+@eval function chunk_mode_jvp(f!::F, y, x, cfg::JVPConfig{T,V,N}) where {F,T,V,N}
+    $(jvp_chunk_mode_expr(quote
                                    ydual, xdual = cfg.duals
                                    seed!(xdual, x)
                                end,
@@ -231,8 +235,8 @@ end
                                :(map!(d -> value(T,d), y, ydual))))
 end
 
-@eval function chunk_mode_jacobian!(result, f::F, x, cfg::JacobianConfig{T,V,N}) where {F,T,V,N}
-    $(jacobian_chunk_mode_expr(quote
+@eval function chunk_mode_jvp!(result, f::F, x, cfg::JVPConfig{T,V,N}) where {F,T,V,N}
+    $(jvp_chunk_mode_expr(quote
                                    xdual = cfg.duals
                                    seed!(xdual, x)
                                end,
@@ -241,8 +245,8 @@ end
                                :(extract_value!(T, result, ydual))))
 end
 
-@eval function chunk_mode_jacobian!(result, f!::F, y, x, cfg::JacobianConfig{T,V,N}) where {F,T,V,N}
-    $(jacobian_chunk_mode_expr(quote
+@eval function chunk_mode_jvp!(result, f!::F, y, x, cfg::JVPConfig{T,V,N}) where {F,T,V,N}
+    $(jvp_chunk_mode_expr(quote
                                    ydual, xdual = cfg.duals
                                    seed!(xdual, x)
                                end,
